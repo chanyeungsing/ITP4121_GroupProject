da3880ec9387a42c7957e90da5622384326cf83c
Edward Chan <112676950+chanyeungsing@users.noreply.github.com>
2024-04-06 03:59:15 +0000

    vpc & sg

--- azure/main.tf
+++ azure/main.tf
@@ -18,3 +18,30 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "westus2"
 }
+
+#vpc
+resource "azurerm_virtual_network" "vpc1" {
+  name                = "vpc1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.sg1.id
+  }
+}
+
+#Security Group
+resource "azurerm_network_security_group" "sg1" {
+  name                = "sg1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
