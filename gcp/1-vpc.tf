5dc0c03422b33225662f9d30d2ec5259c773eeed
Edward Chan <112676950+chanyeungsing@users.noreply.github.com>
2024-04-10 05:32:19 +0000

    gcp: 2 subnet

--- gcp/1-vpc.tf
+++ gcp/1-vpc.tf
@@ -1,5 +1,21 @@
 resource "google_compute_network" "vpc2" {
-  name = "vpc2"
+  name    = "vpc2"
   project = "fyp-a08"
+  auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "subnet3" {
+  project       = "fyp-a08"
+  name          = "subnet3"
+  ip_cidr_range = "10.0.3.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc2.id
+}
+
+resource "google_compute_subnetwork" "subnet4" {
+  project = "fyp-a08"
+  name          = "subnet4"
+  ip_cidr_range = "10.0.4.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc2.id
+}
